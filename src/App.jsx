601f9d8074890f8ed8376b3d7299956888f8e7f2
Rodrigo Lopes <rodrigoczlopes@gmail.com>
2022-11-22 17:13:45 -0300

    modificação time

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 const Mostrarnumero = (props) => {
   return(
     <p>
-    {props.numero}
-
+    {props.numero}<br/>
+    Voltas
     </p>
   )
 }
-const tempo = (props) => {
+const MostrarTempo = (props) => {
   return(
     <p>
-    {props.tempo}
+    {props.tempo} <br/>
+    Tempo
     </p>
   )
 }
@@ -20,7 +21,26 @@ const Button = (props) => <button onClick={props.onClick}>{props.text}</button>
 
 function App() {
   const [numeroVoltas, setNumero] = useState(0);
+  const [running, setRunning] = useState(false);
   const [tempo, setTempo] = useState(0);
+  //setInterval é uma função do javascript que chama a função a cada determinado tempo
+  useEffect(() => {
+    let timer = null
+    if (running) {
+      timer = setInterval ( () =>{
+        setTempo(old => old + 1)
+      },1000 )
+    }
+    return () => {
+      if (timer) {
+        clearInterval(timer)
+      }
+    }
+  }, [running])
+   
+  const toggleRunning = () => {
+    setRunning(!running)
+  }
 
   const incrementNumeroVoltas = () =>{
     setNumero(numeroVoltas + 1)
@@ -28,23 +48,14 @@ function App() {
   const decrementNumeroVoltas = () =>{
     setNumero(numeroVoltas - 1)
   }
-  const resetNumeroVoltas = () => {
-    setNumero(numeroVoltas = 0)
-  }
   
   return (
     <div>
-
-      {numeroVoltas} <br/>
-      Voltas<br/>
-
+    <Mostrarnumero numero={numeroVoltas} /> 
     <Button onClick={incrementNumeroVoltas} text='+' />
     <Button onClick={decrementNumeroVoltas} text='-' /> <br/>
-    <p>
-      {tempo}<br/>
-      Tempo
-    </p>
-    <Button text='Iniciar'/>
+    <MostrarTempo tempo={tempo} />
+    <Button onClick={toggleRunning} text='Iniciar'/>
     <Button text='Reiniciar' />
     </div>
   )
